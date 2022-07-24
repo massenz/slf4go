0cd6372f86baf480d5f05cf876f323f3deaa4939
Marco Massenzio <m@massenz.io>
2022-07-23 23:03:37 -0700

    Initial implementation of externalized logging configuration

--- example.go
+++ example.go
@@ -18,28 +18,33 @@
 package main
 
 import (
-	"flag"
-	"github.com/massenz/slf4go/logging"
+    "flag"
+    "github.com/massenz/slf4go/logging"
 )
 
 func main() {
-	logging.RootLog.Info("Program started - before any logs are available")
-	var log = logging.NewLog("example")
-	defer log.Trace("Exiting")
+    logging.RootLog.Info("Program started - before any loggers are available")
+    logging.RootLog.Debug("This only gets printed out if the config YAML sets root to be DEBUG")
 
-	trace := flag.Bool("trace", false, "If set it will emit trace logs")
-	flag.Parse()
-	if *trace {
-		log.Level = logging.TRACE
-	}
-	log.Info("An INFO message")
-	log.Debug("This will NOT be logged, unless -trace is given")
+    // This log will send all its output to /tmp/example.log (see the log_config.yaml file)
+    var log = logging.NewLog("example")
+    defer log.Trace("Exiting")
 
-	if !*trace {
-		log.Level = logging.DEBUG
-	}
-	log.Debug("This WILL be printed out")
-	log.Info("The `trace` on exit will only be visible with the -trace option")
+    trace := flag.Bool("trace", false, "If set it will emit trace logs")
+    flag.Parse()
+    if *trace {
+        log.Level = logging.TRACE
+    }
+    log.Info("An INFO message")
+    log.Debug("This will NOT be logged, unless -trace is given")
 
-	logging.NullLog.Error("No one will ever see this, like, ever 😤")
+    if !*trace {
+        log.Level = logging.DEBUG
+        log.Debug("Setting the example logger to DEBUG, if -trace is not set")
+    }
+    log.Info("The `trace` on exit will only be visible with the -trace option")
+
+    nullLog := logging.NewLog("null")
+    nullLog.Level = logging.NONE
+    nullLog.Error("No one will ever see this, like, ever 😤")
 }
